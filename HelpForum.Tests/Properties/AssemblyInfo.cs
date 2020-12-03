2160ee05c05b0936122d8f3a1519ab2a9fb58ba1
Robert McDonnell <rcmcd@hotmail.com>
2020-12-02 23:33:07 -0700

    Added ASP.NET MVC website. Added tests for website.

---
+++ HelpForum.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelpForum.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelpForum.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0c961d8-4563-4609-b85f-0f3c62123200")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelpForum.Tests/RolesTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Security;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelpForum.Tests
+{
+    [TestClass]
+    public class RolesTests
+    {
+        [TestMethod]
+        public void Roles_RegisteredUser()
+        {
+            Roles.Enabled = true;
+            var isUserInRole = Roles.IsUserInRole("username@email.com", "AuthenticatedUser");
+            Assert.IsTrue(isUserInRole);
+        }
+    }
+}
